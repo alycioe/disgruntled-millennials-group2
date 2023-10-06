58a63c060b18048ee584e733f4920a03424b7c66
allysonpacheco@yahoo.com <allysonpacheco@yahoo.com>
2023-10-05 20:26:08 -0400

    Added controller elements with some minor outlining

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,2 @@
-const router =  require('express').Router();
-
-const homeRoutes = require('./homeRoutes');
-const apiRoutes = require('./api');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
 
 module.exports = router;
