2a056d5e47f3e5e3d5cc84979c8095f3edd1995e
jbird11801@gmail.com <jbird11801@gmail.com>
2023-10-05 21:40:23 -0400

    feature/public

---
+++ public/js/login.js
@@ -0,0 +1,43 @@
+
+//handles loging in
+
+const loginFormHandler = async (event) => {
+
+    event.preventDefault();
+  
+    // Collects login info
+
+    const username = document.querySelector('#login-identifier').value.trim();
+
+    const password = document.querySelector('#login-password').value.trim();
+
+
+    if (username && password) {
+
+      console.log (username + password + " login"); // log user in here
+
+    }
+
+  };
+
+
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    
+    console.log("sign up"); // send user to sign up page 
+    
+  };
+
+// listeners
+
+document
+.querySelector('#login-form')
+.addEventListener('submit', loginFormHandler);
+
+document
+.querySelector('#toggle-form')
+.addEventListener('click', signupFormHandler);
+
+  
