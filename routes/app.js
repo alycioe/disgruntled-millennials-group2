af39659787c09e5fb64810c65aa46d2b8d8d690d
jbird11801@gmail.com <jbird11801@gmail.com>
2023-10-05 21:40:23 -0400

    feature/public

--- routes/app.js
+++ routes/app.js
@@ -1,1 +1,9 @@
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 
+app.use('/', indexRouter);
+app.use('/', authRouter);
+
+
+app.use('/', indexRouter);
+app.use('/', authRouter);
