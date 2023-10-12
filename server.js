02e00268fc1638f6b19d4691e6b3b63bc23b3da6
jbird11801@gmail.com <jbird11801@gmail.com>
2023-10-12 19:32:57 -0400

    feature/controllerLink

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const multer = require('multer');  // Multer for handling file uploads
-const routes = require('./controllers');
 const { POST } = require('./model');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
