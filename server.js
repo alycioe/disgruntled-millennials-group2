649d44572ad7cdbbaf15216598917e775b3ddb40
alocke <alocke.games@gmail.com>
2023-10-13 22:31:35 -0400

    handlebars work, no css on the dashboard

--- server.js
+++ server.js
@@ -9,28 +9,46 @@ const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//you can delete these limes 12 - 20 just so you can see the log in / sign up page 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/login', async (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/login.html'))
+// MAKE SURE TO INSTALL EXPRESS-HANDLEBARS
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+  })
+);
+app.set('view engine', 'handlebars');
+
+
+app.get('/login', (req, res) => {
+  //res.sendFile(path.join(__dirname, '/views/login.html'))
+  res.render('login', {
+  layout: 'main',
+  });
  });
 
- app.get('/signup', async (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/signup.html'))
+ app.get('/signup', (req, res) => {
+  //res.sendFile(path.join(__dirname, '/views/signup.html'))
+  res.render('signup', {
+    layout: 'main',
+    });
+ });
+
+ app.get('/dashboard', (req, res) => {
+  //res.sendFile(path.join(__dirname, '/views/signup.html'))
+  res.render('dashboard', {
+    layout: 'main',
+    });
  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use(routes);
-
 
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
