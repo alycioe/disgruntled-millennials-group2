09adc888037dcb128dd73a6b01e901cfb8c69828
jbird11801@gmail.com <jbird11801@gmail.com>
2023-10-15 13:01:27 -0400

    feature/PostsSave

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,28 +2,72 @@
 // postRoutes.js
 
 const router = require('express').Router();
-const { Post, User } = require('../../model');
+const { Post } = require('../../model');
+const fs = require('fs');
 
 // Route for text posts (no image)
 router.post('/create-text-post', async (req, res) => {
-  // Extract text content from the request body
-  const { text } = req.body;
 
-  // Create a new post record in the database
-  Post.create({
-    userName: 'User Name',
-    text: text,
-    user_id: 1, //IDK WHAT TO DO WITH THIS
-  })
-    .then((post) => {
-      // Post creation successful
-      // REDIRECT TO DASHBOARD...?
-      res.redirect('/'); //
-    })
-    .catch((err) => {
-      console.error('Error creating text post:', err);
-      res.status(500).send('Error creating text post');
-    })
+  // Extract text content from the request body
+
+  const newPost = req.body;
+
+  fs.readFile('./seeds/postData.json', 'utf8', async (err, data) => {
+
+    if (err) {
+
+      console.error(err);
+
+  } else {
+
+      // Convert string into JSON object
+
+      const parsedPosts = JSON.parse(data);
+
+      // Add a new note
+
+        var post = await Post.create({
+
+          userName : newPost.userName,
+
+          text : newPost.text
+          
+        })
+
+      .then(() => {
+
+        console.log(JSON.stringify(post))
+
+        parsedPosts.push(newPost);
+
+        // Write updated reviews back to the file
+
+        fs.writeFile('./seeds/postData.json', JSON.stringify(parsedPosts, null, 4),(writeErr) =>
+
+          writeErr
+
+            ? console.error(writeErr)
+
+            : console.info('Successfully updated Posts!')
+
+
+      )
+
+        // Post creation successful
+        // REDIRECT TO DASHBOARD...?
+
+        res.redirect('/dashboard'); //
+
+      })
+      
+      .catch((err) => {
+        console.error('Error creating text post:', err);
+        res.status(500).send('Error creating text post');
+      })
+
+    }
+
+});
 
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const sequelize = require('./config/connection');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const path = require('path');
@@ -9,22 +9,22 @@ const { POST } = require('./model');
 const routes = require('./controllers');
 
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'myLInne14L5', // just a pun on the word millennials
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'myLInne14L5', // just a pun on the word millennials
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -81,8 +81,8 @@ const upload = multer({ storage: storage });
 const hbs = exphbs.create();
 
 
-// sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
 });
-// });
+});
